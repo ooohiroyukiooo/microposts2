8cd5d3e8f751293ac31e432b116c1df4bc60f8ae
ooohiroyukiooo <hiroyuki.takayama1985@gmail.com>
2015-08-08 16:21:22 +0000

    merge user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,21 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+     @user = User.find(current_user)
+  end
+  
+  def update
+     @user = User.find(current_user)
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: '基本情報を編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
